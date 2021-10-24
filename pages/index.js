c7674baae9c8ba33eec16d1cd2880c3f41ad227e
Carter Chavez <Carter.chavez17@gmail.com>
2021-10-24 12:19:23 -0700

    udpate page header

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Carter Chavez | Web &amp; iOS Dev</title>
+        <title>Carter Chavez</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
