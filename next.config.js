11c3d55adb77ee7fa6259bab74f632fd592c6e53
Nicolas Pita <pitanicolase@gmail.com>
2021-10-24 11:48:59 -0700

    new new

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const withImages = require('next-images');
 
-(module.exports = withImages({
-  fileExtensions: ['jpg', 'jpeg', 'png', 'gif', 'mp4'],
-  webpack(config, options) {
-    return config;
+module.exports = withImages(
+  {
+    fileExtensions: ['jpg', 'jpeg', 'png', 'gif', 'mp4'],
+    webpack(config, options) {
+      return config;
+    },
   },
-})),
-  { assetPrefix: '/devsite/' };
+  { assetPrefix: '/devsite/' }
+);
