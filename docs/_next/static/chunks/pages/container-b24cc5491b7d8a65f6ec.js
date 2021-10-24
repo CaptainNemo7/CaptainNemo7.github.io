7bb8c829e69d172495288b9fdc0ee4b44a57e8af
Carter Chavez <Carter.chavez17@gmail.com>
2021-10-24 12:23:12 -0700

    update style

--- docs/_next/static/chunks/pages/container-b24cc5491b7d8a65f6ec.js
+++
@@ -1,1 +0,0 @@
-_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[6],{GhZ5:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/container",function(){return o("bff6")}])},bff6:function(n,_){}},[["GhZ5",0]]]);
